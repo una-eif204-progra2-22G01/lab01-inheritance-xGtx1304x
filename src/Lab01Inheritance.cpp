b03057c9b896dd6f75b210ea5e6765953afc079a
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2022-03-15 20:09:37 -0600

    Correct the main file

--- src/Lab01Inheritance.cpp
+++ src/Lab01Inheritance.cpp
@@ -1,29 +1,13 @@
-#include <iostream>  // allows program to output data to the screen
-
-struct Base {
-    virtual void do_something() {}
-};
-
-struct Derived1 : public Base {
-    virtual void do_something() {
-        std::cout << "Derived1!!!" << std::endl;
-    }
-};
-
-struct Derived2 : public Base {
-    virtual void do_something() {
-        std::cout << "Derived2!!!" << std::endl;
-    }
-};
+#include "Derived2.h"
 
 // function main begins program execution
 int main(int argc, const char *argv[]) {
     std::cout << "Welcome to the UNA!" << std::endl;
 
     Derived1 derived1;
-    derived1.do_something();
+    derived1.doSomething();
 
     Derived2 derived2;
-    derived2.do_something();
+    derived2.doSomething();
 
 }  // end function main
