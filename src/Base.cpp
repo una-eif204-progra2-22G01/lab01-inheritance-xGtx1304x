c9f7c9fe40e3da67e4b3352f1cc77b33f09613da
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2022-03-15 19:50:40 -0600

    Creation of class Base

---
+++ src/Base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andres on 15/3/2022.
+//
+
+#include "Base.h"
---
+++ src/Base.h
@@ -0,0 +1,17 @@
+//
+// Created by Andres on 15/3/2022.
+//
+
+#ifndef LAB01_INHERITANCE_XGTX1304X_BASE_H
+#define LAB01_INHERITANCE_XGTX1304X_BASE_H
+
+
+class Base {
+public:
+    Base() = default;
+    virtual ~Base() = default;
+    virtual void doSomething() = 0;
+};
+
+
+#endif //LAB01_INHERITANCE_XGTX1304X_BASE_H
