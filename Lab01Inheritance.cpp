4415b43e69e2c56227889d4c90e088bde62d7008
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2022-03-15 19:10:18 -0600

    Correct grammar errors

--- Lab01Inheritance.cpp
+++ Lab01Inheritance.cpp
@@ -4,14 +4,14 @@ struct Base {
     virtual void do_something() {}
 };
 
-struct Derived1 : Base {
-    virtual void do_semothing() {
+struct Derived1 : public Base {
+    virtual void do_something() {
         std::cout << "Derived1!!!" << std::endl;
     }
 };
 
-struct Derived2 : Base {
-    virtual void do_semothing() {
+struct Derived2 : public Base {
+    virtual void do_something() {
         std::cout << "Derived2!!!" << std::endl;
     }
 };
@@ -21,9 +21,9 @@ int main(int argc, const char *argv[]) {
     std::cout << "Welcome to the UNA!" << std::endl;
 
     Derived1 derived1;
-    derived1.do_semothing();
+    derived1.do_something();
 
     Derived2 derived2;
-    derived2.do_semothing();
+    derived2.do_something();
 
 }  // end function main
